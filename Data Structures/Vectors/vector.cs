cb5d06775d8493a3566f5172ff3d77de08f79b79
Baaart <13395008+Baaartosz@users.noreply.github.com>
2022-03-04 12:59:42 +0000

    Started writing Vector class from scratch.

---
+++ Data Structures/Vectors/vector.cs
@@ -0,0 +1,19 @@
+// Simple Vector Array holding integers for now.
+
+class Vector{
+
+    private int size, capacity;
+
+    public int size(){}
+    public int capacity(){}
+    public bool is_empty(){}
+    public int at(int index){}
+    public void insert(int index, int item){}
+    public void prepend(int item){}
+    public void pop(){}
+    public void delete(int index){}
+    public void remove(int item){}
+    
+    public int find(int item){}
+    private void resize(int newCapacity){}
+}
