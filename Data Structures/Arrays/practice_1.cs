d9b6da2c8516d8a909de0ca9a56d802b2b2da140
bart <hey@baaart.dev>
2022-03-07 23:15:15 +0000

    previous commit did not add new files.

---
+++ Data Structures/Arrays/practice_1.cs
@@ -0,0 +1,13 @@
+/*
+IsUnique()
+
+Implement an algorithim to determine if a string has all unique characters.
+
+What if you cannot use addiontal data structures?
+
+baaart.dev
+*/ 
+
+bool IsUnique(){
+    return null;
+}
