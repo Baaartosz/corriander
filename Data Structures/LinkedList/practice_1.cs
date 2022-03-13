0fb2ee50142faa4f520a2142b0ce8186e62f2a1c
Baaartosz <bartosz_p@hotmail.com>
2022-03-13 19:09:15 +0000

    Created question file

---
+++ Data Structures/LinkedList/practice_1.cs
@@ -0,0 +1,22 @@
+/*
+Remove Duplicates 
+
+Write code to remove duplicates from an unsorted linked list.
+
+Follow up with; How would you solve this problem without using a temporary buffer?
+
+baaart.dev
+*/
+
+// Assuming a LinkedList of integers.
+
+//RemoveDuplicates(LinkedList.head);
+
+void RemoveDuplicates(ListNode node){
+
+}
+
+
+void RemoveDuplicates_NoBuffer(ListNode node){
+    
+}
