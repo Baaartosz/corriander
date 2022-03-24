d98753f8f78b0447caeeaba0770063c8d54bf48a
Baaartosz <bartosz_p@hotmail.com>
2022-03-24 19:38:15 +0000

    Described Solutions, will implement tomorrow

--- Data Structures/Stack & Queue/practice_1.cs
+++ Data Structures/Stack & Queue/practice_1.cs
@@ -5,3 +5,22 @@ Describe how you could use a single array to implement three stacks.
 
 baaart.dev
 */
+
+/*
+
+Approach 1 ( Fixed Division ) [Simple]
+
+Equally Divide the Array into three equal parts.
+
++ relitivity simple approach
+- One Stack can run out of capacity while others can be empty.
+
+Approach 2 ( Flexible Stack Size ) [Complex]
+
+Circually Linked Array
+Shifting nodes when capacity is exceeded.
+
+- Complex approach requiring long bits of code
++ Allows for flexible capacity of the three stacks optimsing for the best memory usage.
+
+*/
