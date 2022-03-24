5d059b54fe10bd4da0262da8093bd838b4191924
Baaartosz <bartosz_p@hotmail.com>
2022-03-24 18:33:13 +0000

    Implemented Stack data structure

---
+++ Data Structures/Stack & Queue/stack.cs
@@ -0,0 +1,42 @@
+/*
+My implementation of a Stack Data Structure.
+baaart.dev
+*/
+
+public class Stack<T> {
+
+    private static class StackNode<T> {
+        private T data;
+        private StackNode<T> next;
+
+        public StackNode(T data){
+            this.data = data;
+        }
+    }
+
+    private StackNode<T> top;
+
+    public T Pop(){
+        if(top == null) throw new EmptyStackException();
+        
+        T item = top.data;
+        top = top.next;
+        
+        return item;
+    }
+
+    public void Push(T data){
+        StackNode<T> t = new StackNode<T>(data);
+        t.next = top;
+        top = t;
+    }
+
+    public T peek(){
+        if(top == null) throw new EmptyStackException();
+        return top.data;
+    }
+
+    public bool IsEmpty(){
+        return top == null;
+    }
+}
