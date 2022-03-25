5ac646634e65c735b462b08e3e6d89b80191dac2
Baaartosz <bartosz_p@hotmail.com>
2022-03-25 17:44:24 +0000

    q2 almost done, testing required.

--- Data Structures/Stack & Queue/practice_2.cs
+++ Data Structures/Stack & Queue/practice_2.cs
@@ -8,3 +8,52 @@ push(), pop() and min() should all operate at O(1) time.
 
 baart.dev
 */
+
+
+public class Stack {
+    private class StackNode {
+        internal int data;
+        internal StackNode next;
+        internal int subStackMin; // Keep track of sub-stack's minimum
+
+        public StackNode(int data){
+            this.data = data;
+        }
+    }
+
+    private StackNode top = null;
+
+    public int Pop(){
+        if(top == null) throw new EmptyStackException();
+        
+        int item = top.data;
+        top = top.next;
+        
+        return item;
+    }
+
+    public void Push(int data){
+        StackNode t = new StackNode(data);
+        t.next = top;
+        if(top.subStackMin == null) {
+            t.subStackMin = data;
+        } else {
+            t.subStackMin = top.subStackMin; // add min / new minimum
+        }
+        top = t;
+        
+    }
+
+    public int Peek(){
+        if(top == null) throw new EmptyStackException();
+        return top.data;
+    }
+
+    public int Min(){
+        return top.subStackMin;
+    }
+
+    public bool IsEmpty(){
+        return top == null;
+    }
+}
