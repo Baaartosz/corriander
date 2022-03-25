9bad22d4bf6120b5566fc047c5ff968fd21122f8
Baaartosz <bartosz_p@hotmail.com>
2022-03-25 16:29:03 +0000

    q2 stacks & queues

---
+++ Data Structures/Stack & Queue/practice_2.cs
@@ -0,0 +1,10 @@
+/*
+Stack Min
+
+How could you design a Stack which, In addition to push() and pop() has a function min()
+which returns the minimum element?
+
+push(), pop() and min() should all operate at O(1) time.
+
+baart.dev
+*/
