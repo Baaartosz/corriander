d2548745da4a23a661953a5ad891e07f64fd391c
Baaartosz <bartosz_p@hotmail.com>
2022-03-26 10:24:17 +0000

    beginning question3

---
+++ Data Structures/Stack & Queue/practice_3.cs
@@ -0,0 +1,19 @@
+/*
+Stack of Plates
+
+Imagine a (literal) stack of plates. If the stack gets too high, it might topple. 
+Thereforem in real-life, we would likely start a new stack when the previous stack 
+exceeds some threshold.
+
+Implement a data structure 'SetOfStacks' that mimics this.
+
+'SetOfStacks' should be composed of several stacks once the previous one exceeds capacity.
+SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack.
+
+FOLLOW UP
+---------
+
+Implement a function popAt(int index) which preforms a pop operation at a specific sub-stack.
+
+baaart.dev
+*/
