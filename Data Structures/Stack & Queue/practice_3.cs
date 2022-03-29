1a567b4a9f7affed294cc25bbbc60a87d722af5e
Baaart <13395008+Baaartosz@users.noreply.github.com>
2022-03-29 23:51:10 +0100

    Finished implementation

--- Data Structures/Stack & Queue/practice_3.cs
+++ Data Structures/Stack & Queue/practice_3.cs
@@ -1,102 +1,75 @@
-/*
-Stack of Plates
+    /*
+    Stack of Plates
 
-Imagine a (literal) stack of plates. If the stack gets too high, it might topple. 
-Therefore in real-life, we would likely start a new stack when the previous stack 
-exceeds some threshold.
+    Imagine a (literal) stack of plates. If the stack gets too high, it might topple. 
+    Therefore in real-life, we would likely start a new stack when the previous stack 
+    exceeds some threshold.
 
-Implement a data structure 'SetOfStacks' that mimics this.
+    Implement a data structure 'SetOfStacks' that mimics this.
 
-'SetOfStacks' should be composed of several stacks once the previous one exceeds capacity.
-SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack.
+    'SetOfStacks' should be composed of several stacks once the previous one exceeds capacity.
+    SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack.
 
-FOLLOW UP
----------
+    FOLLOW UP
+    ---------
 
-Implement a function popAt(int index) which preforms a pop operation at a specific sub-stack.
+    Implement a function popAt(int index) which preforms a pop operation at a specific sub-stack.
 
-baaart.dev
-*/
-
-public class SetOfStacks {
-    
-    int stackCapacity = 30; // How much a Stack can hold before being starting new one.
-    int stackIndex = 0;
-    Stack<T>[] stacks; // this 
-    // New stacks created in this array for access.
-    public T Pop(){
-        if(stacks[stackIndex].IsEmpty) throw new System.Exception("Stack Empty");
-        T value = stacks[stackInde].Pop();
-        return value;
-    }
-    /* FOLLOW UP
-        public T popAt(int index){}
+    baaart.dev
     */
-    public void Push(T data){
-        if(stacks[stackIndex].IsFull) newStack();
-        stacks[stackIndex].Push(data);
-    }
-    public T Peek(){
-        return stacks[stackIndex].Peek();
-    }
 
-    private void newStack(){
-        stackIndex++;
-        stacks[stackIndex] = new Stack<T>(stackCapacity);
-    }
-    // increment stack index
-    // add new Stack Object to Stack<T> array.
+using System;
 
-    private void destroyStack(){
+namespace SetOfStacks_Sandbox
+{
+    public class SetOfStacks<T>
+    {
+        private Stack<Stack<T>> _subStacks = new Stack<Stack<T>>();
+        private int _capacity; // Amount of StackNodes kept in a single Stack.
+        private int _items = 0; // Keep track of items in current Stack.
 
-    }
-    
+        public SetOfStacks(int capacity)
+        {
+            _capacity = capacity;
+        }
 
-    public boolean IsEmpty(){
-        return stacks[0].IsEmpty; // Only need to check if Stack 0 is empty;
-    }
-    // returns if stacks are empty.
-}
+        private void NewStack()
+        {
+            _subStacks.Push(new Stack<T>());
+        }
+        private void DestroyStack()
+        {
+            _subStacks.Pop();
+        }
+        
+        public T Pop()
+        {
+            // Check if the sub-stack is empty.
+            if(_subStacks.Peek().IsEmpty()) DestroyStack();
+            if (_subStacks.IsEmpty()) throw new Exception("SetOfStacks is Empty");
+            T value = _subStacks.Peek().Pop();
+            _items--;
+            return value;
+        }
 
-/*
-My implementation of a Stack Data Structure 
-using an Array
-baaart.dev
-*/
-public class Stack<T> {
+        public void Push(T data)
+        {
+            if(_items == _capacity) NewStack();
+            if (IsEmpty()) NewStack();
+            _subStacks.Peek().Push(data);
+            _items++;
+        }
 
-    private T[] stackArray;
-    private int capacity;
-    private int items = 0;
-    public Stack(int capacity){
-        this.capacity = capacity;
-        stackArray = new T[capacity];
-    }
+        public T Peek()
+        {
+            if (_subStacks.IsEmpty()) throw new Exception("SetOfStacks is Empty");
+            if (_subStacks.Peek().IsEmpty()) throw new Exception("SubStack is Empty.");
+            return _subStacks.Peek().Peek();
+        }
 
-    public T Pop(){
-        if(IsEmpty()) throw new EmptyStackException();
-        T value = stackArray[items-1];
-        stackArray[items-1] = null;
-        items--;
-        return value;
-    }
-
-    public void Push(T data){
-        if(IsFull()) throw new FullStackException();
-        stackArray[items] = data;
-        items++;
-    }
-
-    public T Peek(){
-        if(items == 0) throw new EmptyStackException();
-        return stackArray[items-1];
-    }
-
-    public bool IsEmpty(){
-        return items == 0;
-    }
-
-    public bool IsFull(){
-        return items == capacity;
+        public bool IsEmpty()
+        {
+            return _subStacks.IsEmpty();
+        }
     }
 }
