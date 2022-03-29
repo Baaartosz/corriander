6ca1f5b5cbed81ef34df22a7b012ad84415e5159
Baaart <13395008+Baaartosz@users.noreply.github.com>
2022-03-29 19:34:00 +0100

    majority finished, needs recoding from an Array as storage for stacks.

--- Data Structures/Stack & Queue/practice_3.cs
+++ Data Structures/Stack & Queue/practice_3.cs
@@ -2,7 +2,7 @@
 Stack of Plates
 
 Imagine a (literal) stack of plates. If the stack gets too high, it might topple. 
-Thereforem in real-life, we would likely start a new stack when the previous stack 
+Therefore in real-life, we would likely start a new stack when the previous stack 
 exceeds some threshold.
 
 Implement a data structure 'SetOfStacks' that mimics this.
@@ -18,27 +18,43 @@ Implement a function popAt(int index) which preforms a pop operation at a specif
 baaart.dev
 */
 
-public class StackOfPlates {
+public class SetOfStacks {
     
     int stackCapacity = 30; // How much a Stack can hold before being starting new one.
     int stackIndex = 0;
-    Stack<T>[] stacks;
-    int[] sizes;
+    Stack<T>[] stacks; // this 
     // New stacks created in this array for access.
-    public T pop(){}
-    // FOLLOW UP
-    // public T popAt(int index){}
-    public void push(T data){}
-    public T peek(){}
+    public T Pop(){
+        if(stacks[stackIndex].IsEmpty) throw new System.Exception("Stack Empty");
+        T value = stacks[stackInde].Pop();
+        return value;
+    }
+    /* FOLLOW UP
+        public T popAt(int index){}
+    */
+    public void Push(T data){
+        if(stacks[stackIndex].IsFull) newStack();
+        stacks[stackIndex].Push(data);
+    }
+    public T Peek(){
+        return stacks[stackIndex].Peek();
+    }
 
-    private void newStack(){}
+    private void newStack(){
+        stackIndex++;
+        stacks[stackIndex] = new Stack<T>(stackCapacity);
+    }
     // increment stack index
     // add new Stack Object to Stack<T> array.
 
-    private void destroyStack(){}
-    // save tmp var of stackIndex
+    private void destroyStack(){
 
-    public boolean IsEmpty(){}
+    }
+    
+
+    public boolean IsEmpty(){
+        return stacks[0].IsEmpty; // Only need to check if Stack 0 is empty;
+    }
     // returns if stacks are empty.
 }
 
@@ -58,26 +74,29 @@ public class Stack<T> {
     }
 
     public T Pop(){
-        if(top == null) throw new EmptyStackException();
-        
-        T item = top.data;
-        top = top.next;
-        
-        return item;
+        if(IsEmpty()) throw new EmptyStackException();
+        T value = stackArray[items-1];
+        stackArray[items-1] = null;
+        items--;
+        return value;
     }
 
     public void Push(T data){
-        if(items == capacity) throw new FullStackException()
+        if(IsFull()) throw new FullStackException();
         stackArray[items] = data;
         items++;
     }
 
-    public T peek(){
+    public T Peek(){
         if(items == 0) throw new EmptyStackException();
         return stackArray[items-1];
     }
 
     public bool IsEmpty(){
         return items == 0;
+    }
+
+    public bool IsFull(){
+        return items == capacity;
     }
 }
