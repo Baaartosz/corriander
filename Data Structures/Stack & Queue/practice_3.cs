de06a8203129187799f1b19d9bfbaa23854a00f0
Baaart <13395008+Baaartosz@users.noreply.github.com>
2022-03-30 11:36:37 +0100

    duplicate class, replaced with actual Stack<T>

--- Data Structures/Stack & Queue/practice_3.cs
+++ Data Structures/Stack & Queue/practice_3.cs
@@ -1,22 +1,22 @@
-    /*
-    Stack of Plates
+/*
+Stack of Plates
 
-    Imagine a (literal) stack of plates. If the stack gets too high, it might topple. 
-    Therefore in real-life, we would likely start a new stack when the previous stack 
-    exceeds some threshold.
+Imagine a (literal) stack of plates. If the stack gets too high, it might topple. 
+Therefore in real-life, we would likely start a new stack when the previous stack 
+exceeds some threshold.
 
-    Implement a data structure 'SetOfStacks' that mimics this.
+Implement a data structure 'SetOfStacks' that mimics this.
 
-    'SetOfStacks' should be composed of several stacks once the previous one exceeds capacity.
-    SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack.
+'SetOfStacks' should be composed of several stacks once the previous one exceeds capacity.
+SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack.
 
-    FOLLOW UP
-    ---------
+FOLLOW UP
+---------
 
-    Implement a function popAt(int index) which preforms a pop operation at a specific sub-stack.
+Implement a function popAt(int index) which preforms a pop operation at a specific sub-stack.
 
-    baaart.dev
-    */
+baaart.dev
+*/ 
 
 using System;
 
@@ -73,58 +73,49 @@ namespace SetOfStacks_Sandbox
         }
     }
 
-    public class SetOfStacks<T>
+    public class Stack<T> : IStack<T>
     {
-        private Stack<Stack<T>> _subStacks = new Stack<Stack<T>>();
-        private int _capacity; // Amount of StackNodes kept in a single Stack.
-        private int _items = 0; // Keep track of items in current Stack.
+        public class StackNode<T>
+        {
+            internal T Data;
+            internal StackNode<T> Next;
 
-        public SetOfStacks(int capacity)
-        {
-            _capacity = capacity;
+            public StackNode(T data)
+            {
+                Data = data;
+            }
         }
 
-        private void NewStack()
-        {
-            _subStacks.Push(new Stack<T>());
-        }
-        private void DestroyStack()
-        {
-            _subStacks.Pop();
-        }
+        private static StackNode<T> _top;
         
         public T Pop()
         {
-            // Check if the sub-stack is empty.
-            if(_subStacks.Peek().IsEmpty()) DestroyStack();
-            if (_subStacks.IsEmpty()) throw new Exception("SetOfStacks is Empty");
-            T value = _subStacks.Peek().Pop();
-            _items--;
-            return value;
+            if (_top == null) throw new Exception("Empty Stack");
+        
+            T item = _top.Data;
+            _top = _top.Next;
+        
+            return item;
         }
 
-        public void Push(T data)
-        {
-            if(_items == _capacity) NewStack();
-            if (IsEmpty()) NewStack();
-            _subStacks.Peek().Push(data);
-            _items++;
+        public void Push(T data){
+            StackNode<T> t = new StackNode<T>(data);
+            t.Next = _top;
+            _top = t;
         }
 
-        public T Peek()
-        {
-            if (_subStacks.IsEmpty()) throw new Exception("SetOfStacks is Empty");
-            if (_subStacks.Peek().IsEmpty()) throw new Exception("SubStack is  empty.");
-            return _subStacks.Peek().Peek();
+        public T Peek(){
+            if(_top == null)throw new Exception("Empty Stack");
+            return _top.Data;
         }
 
-        public bool IsEmpty()
-        {
-            return _subStacks.IsEmpty();
+        public bool IsEmpty(){
+            return _top == null;
         }
     }
 
-        /// <summary>
+
+    /// <summary>
     /// Abstract Data Type : Stack Implementation
     /// Additional Extra Info: Stacks are First In, Last Out.
     /// </summary>
