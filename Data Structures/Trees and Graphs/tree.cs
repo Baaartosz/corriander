eb668fff4e2c762cd6b9d4abc45d222a34a199c5
Baaartosz <bartosz_p@hotmail.com>
2022-04-05 10:21:53 +0100

    stopped implementation of Tree/Graph.cs

--- Data Structures/Trees and Graphs/tree.cs
+++ Data Structures/Trees and Graphs/tree.cs
@@ -1,8 +1,16 @@
-public class Tree {
+public interface ITree<T> {
+    public Node<T> root;
+
+    public void Insert(T data);
+    public Node<T> Search(T data);
+}
+
+public class Tree : ITree<T> {
     public Node root;
 }
 
-public class Node{
-    public string name;
-    public Node[] children;
+public class Node<T>{
+    public T data;
+    public Node leftChild;
+    public Node rightChild;
 }
