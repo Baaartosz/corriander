8601f0d7a81b11e0b7ecc4a4544ca174c7c8a671
Baaart <13395008+Baaartosz@users.noreply.github.com>
2022-04-07 14:42:20 +0100

    Cleared testing code for Q3. Updated practice_3.cs for Tree and Graphs.

--- Data Structures/Trees and Graphs/practice_3.cs
+++ Data Structures/Trees and Graphs/practice_3.cs
@@ -11,32 +11,38 @@ baaart.dev
 
 namespace Graph_Sandbox {
 
-    public class TreeUtilDepth {
-
-        public ArrayList ListOfDepths(TreeNode root){
-            return Search(new ArrayList(), root, 0);
+    public class TreeUtilDepth
+    {
+        public Vector<LinkedList<TreeNode>> ListOfDepths(TreeNode root)
+        {
+            return Search(new Vector<LinkedList<TreeNode>>(), root, 0);
         }
 
-        private ArrayList Search(ArrayList levelDepths, TreeNode root, int depth){
-            if(root == null) return; // No tree provided
-            if(levelDepths[depth] == null){ // Create new level.
-                levelDepths[depth].add(new LinkedList<TreeNode>());
+        private Vector<LinkedList<TreeNode>> Search(Vector<LinkedList<TreeNode>> levelDepths, TreeNode root, int depth)
+        {
+            if (root == null) return levelDepths;
+            if (levelDepths._array[depth] == null)
+            {
+                // Create new level.
+                levelDepths.Push(new LinkedList<TreeNode>());
             }
 
-            levelDepths[depth].pushFront(root); // Push root to LinkedList
+            levelDepths._array[depth].AddLast(root); // Push root to LinkedList
 
-            depth++; // increase depth
+            depth++;
+                
+            if (root.HasLeft())
+            {
+                levelDepths = Search(levelDepths, root.left, depth);
 
-            if(root.left != null) {
-                Search(levelDepths, root.left, depth);
             }
-            if(root.right != null){
-                Search(levelDepths, root.right, depth);
+
+            if (root.HasRight())
+            {
+                levelDepths = Search(levelDepths, root.right, depth);
             }
-        
             return levelDepths;
         }
-
     }
 }
 
