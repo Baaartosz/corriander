72c273dbcdeffb37976f9d40c93200c551a473dd
Baaartosz <bartosz_p@hotmail.com>
2022-04-10 20:40:44 +0100

    Finished Q4 in Stacks and Trees.

--- plantpot/Program.cs
+++ plantpot/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using Coriander.Questions.LinkedList;
+﻿using Coriander.Questions.Stack_and_Queues;
 
 namespace Coriander
 {
@@ -8,8 +6,22 @@ namespace Coriander
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("LinkedLists Q5");
-            practice_5.Test();
+            Console.WriteLine("Stacks and Queues Q4");
+
+            var q = new practice_4.MyQueue<int>();
+
+            Console.WriteLine("ENQUEUE");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + ", ");
+                q.Enqueue(i);
+            }
+
+            Console.WriteLine("\nDEQUEUE");
+            for (int i = 0; i < 10; i++)
+            {
+                 Console.Write(q.Dequeue() + " ,");
+            }
         }
     }
 }
---
+++ plantpot/Questions/Stack and Queues/practice_4.cs
@@ -0,0 +1,60 @@
+﻿using Coriander.DataStructures;
+using Coriander.DataStructures.Collection;
+
+namespace Coriander.Questions.Stack_and_Queues;
+
+/*
+ * Queue Via Stacks
+ *
+ * Implement a MyQueue class which implements a queue using two stacks.
+ */
+
+public class practice_4
+{
+    public class MyQueue<T> : IQueue<T>
+    {
+        private MyStack<T> _primary;
+        private MyStack<T> _temp;
+        private MyStack<T>.StackNode<T>? _front = null;
+        private int _items;
+
+        public MyQueue()
+        {
+            _primary = new MyStack<T>();
+            _temp = new MyStack<T>();
+            _items = 0;
+        }
+
+        public void Enqueue(T data)
+        {
+            _primary.Push(data);
+            _items++;
+        }
+
+        public T Dequeue()
+        {
+            while (!_primary.IsEmpty())
+            {
+                if (_items == 1) break;
+                _temp.Push(_primary.Pop());
+                _items--;
+                
+            }
+
+            T oldestItem = _primary.Pop();
+            _items--;
+            
+            while (!_temp.IsEmpty())
+            {
+                _primary.Push(_temp.Pop());
+                _items++;
+            }
+            
+            return oldestItem;
+        }
+
+        public T Front() => _front.data;
+
+        public bool IsEmpty() => _primary.IsEmpty();
+    }
+}
