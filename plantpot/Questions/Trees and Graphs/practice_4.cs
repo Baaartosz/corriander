fb7f8b4d34be2b0614bd77f619531b7f0fcc537e
Baaartosz <bartosz_p@hotmail.com>
2022-04-10 21:47:19 +0100

    Question 4 for Tree and Graphs created.

---
+++ plantpot/Questions/Trees and Graphs/practice_4.cs
@@ -0,0 +1,22 @@
+﻿using Coriander.DataStructures.Collection;
+
+namespace Coriander.Questions.Trees_and_Graphs;
+
+/*
+ * Check Balanced
+ *
+ * Implement a function to check if a binary tree is balanced.
+ * For the purpose of the question; A balanced tree is defined to be a tree such that the heights of the two
+ * sub-trees of any node never differ by more then one.
+ *
+ * baaart.dev
+ */
+
+public class practice_4
+{
+    public bool IsBalanced<T>(Node<T> root)
+    {
+        
+        return false;
+    }
+}
