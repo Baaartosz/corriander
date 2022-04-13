9030c74054922d5ce9071f0ac30abce11faeb1f1
Baaartosz <bartosz_p@hotmail.com>
2022-04-13 19:32:23 +0100

    Finished Question 1

--- plantpot/Questions/Bit Manipulation/practice_1.cs
+++ plantpot/Questions/Bit Manipulation/practice_1.cs
@@ -7,25 +7,41 @@
  * That is, if M = 10011, you can assume that there are at least 5 bits between j and i.
  *
  * EXAMPLE
- * INPUT : N = 10000000000, M = 10011, i = 2, j = 6
+ * INPUT : i , i = 2, j = 6
  * OUTPUT: N = 10001001100 ( 1000 _ 10011 _ 00 )
  */
 
 
 public class practice_1
 {
-    public long Insert(long N, long M, int i, int j)
+    public int Insert(int N, int M, int i, int j)
     {
+        for (int pos = i; pos <= j; pos++)
+        {
+            N = UpdateBit(N, pos, GetBit(M, 0));
+            M >>= 1;
+        }   
         return N;
+    }
+
+    private int GetBit(int n, int i)
+    {
+        return (n >> 0) & 1;
+    }
+    
+    private int UpdateBit(int n, int i, int newValue)
+    {
+        int mask = ~(1 << i);
+        return (n & mask) | (newValue << i);
     }
 
     public static void Test()
     {
         var q = new practice_1();
         Console.WriteLine("");
-        var result = q.Insert(10000000000, 10011, 2, 6);
-        Console.WriteLine("Output: " + result);
-        Console.WriteLine("Insert(10000000000,10011,2,6) == 10001001100 : " + (result == 10001001100));
+        var result = q.Insert(0b10000000000, 0b10011, 2, 6);
+        Console.WriteLine("Output: " + Convert.ToString(result, 2));
+        Console.WriteLine("Insert(10000000000,10011,2,6) == 10001001100 : " + (result == 0b10001001100));
     }
      
 }
