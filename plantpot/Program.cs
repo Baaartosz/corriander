17d75872f2a3c17985a753b4790ff1817b9f582e
Baaartosz <bartosz_p@hotmail.com>
2022-04-13 13:18:28 +0100

    updated planner

--- plantpot/Program.cs
+++ plantpot/Program.cs
@@ -1,13 +1,10 @@
-﻿using Coriander.Questions.Trees_and_Graphs;
-
-namespace Coriander
+﻿namespace Coriander
 {
     internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            practice_4.Test();
         }
     }
 }
