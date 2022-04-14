be47c11eac2b667a8f01b5b36aedf8fab61cfec5
Baaartosz <bartosz_p@hotmail.com>
2022-04-14 13:48:15 +0100

    Another attempt at Question 1. This approach has no for loop.
    Also tested on another bit number, works fine.
    
    Both methods work the same.

--- plantpot/Questions/Bit Manipulation/practice_1.cs
+++ plantpot/Questions/Bit Manipulation/practice_1.cs
@@ -7,22 +7,23 @@
  * That is, if M = 10011, you can assume that there are at least 5 bits between j and i.
  *
  * EXAMPLE
- * INPUT : i , i = 2, j = 6
+ * INPUT : N = 10000000000, M = 10011, i = 2, j = 6
  * OUTPUT: N = 10001001100 ( 1000 _ 10011 _ 00 )
  */
 
 
 public class practice_1
 {
+    // First Attempt
     public int Insert(int N, int M, int i, int j)
     {
-        for (int pos = i; pos <= j; pos++)
+        for (int pos = i; pos <= j; pos++) // O(length of inserting binary number)
         {
             N = UpdateBit(N, pos, GetBit(M, 0));
-            M >>= 1;
+            M >>= 1; 
         }   
         return N;
-    }
+    } 
 
     private int GetBit(int n, int i)
     {
@@ -34,14 +35,44 @@ public class practice_1
         int mask = ~(1 << i);
         return (n & mask) | (newValue << i);
     }
+    
+    // Take another go at this but don't use for loop.
+    // Clear bits between i & j on N
+    // Left shift M to fit into N
+    // Merge together
+    public int InsertV2(int N, int M, int i, int j) // O(1)
+    {
+        // Clear bits between i & j on N.
+        // +1 because otherwise i, j would be on indexes 5 -> 1;
+        // When we -1, we lose the current 1, for the remainder 1's
+        // E.g (0b1000000) - 1 = 0111111;
+        int clearMask = ((1 << j + 1 ) - 1) ^ ((1 << (i + 1 - 1)) - 1);
+        N &= ~clearMask;
+        
+        // Shift M bits into place.
+        M <<= i;
+
+        // Merge together results.
+        N |= M;
+
+        return N;
+    }
 
     public static void Test()
     {
         var q = new practice_1();
         Console.WriteLine("");
         var result = q.Insert(0b10000000000, 0b10011, 2, 6);
-        Console.WriteLine("Output: " + Convert.ToString(result, 2));
+        var resultV2 = q.InsertV2(0b10000000000, 0b10011, 2, 6);
+        Console.WriteLine("Output V1: " + Convert.ToString(result, 2));
         Console.WriteLine("Insert(10000000000,10011,2,6) == 10001001100 : " + (result == 0b10001001100));
+        
+        Console.WriteLine("Output V2: " + Convert.ToString(resultV2, 2));
+        Console.WriteLine("Insert(10000000000,10011,2,6) == 10001001100 : " + (resultV2 == 0b10001001100));
+        
+        var test = q.InsertV2(0b10110011100, 0b10011, 2, 6);
+        Console.WriteLine("Output Final Random Test: " + Convert.ToString(test, 2));
+        Console.WriteLine("Insert(10000000000,10011,2,6) == 10001001100 : " + (test == 0b10111001100));
     }
      
 }
